112bb180297af6d2bb18ff74d9d32eac5100315e
Saheed <sdrfngg640@gmail.com>
2019-04-15 20:22:31 +0100

    refactor code in home, about and help

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -21,4 +21,10 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
+
+  test "should get about" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
+  end
 end
