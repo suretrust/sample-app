0b330d5aa287a3f2ba662aefb586fac9dfe49ae8
Saheed <sdrfngg640@gmail.com>
2019-05-16 16:09:41 +0100

    Add detailed create action to session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
-  def new
+  def new; end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user&.authenticate(params[:session][:password])
+    else
+      flash[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
+
+  def destroy; end
 end
