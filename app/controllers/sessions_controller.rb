29062c908ec4be1997ec67496e9343075d84788b
Saheed <sdrfngg640@gmail.com>
2019-05-16 16:19:08 +0100

    Add logged_in? method to session helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
